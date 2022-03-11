37e363d3b7a030e7cc37deef78f1daa7e2a9b0bb
qwertyzzzzz <57785323+DcoldD@users.noreply.github.com>
2022-03-11 12:30:28 +0400

    feat(Gateway): Новый модуль для HotelAutomationProject #52

--- projects/hotel_automation_project/discovery_evr/src/main/java/nc/unc/ama/discovery_evr/DiscoveryEvrApplication.java
+++ projects/hotel_automation_project/discovery_evr/src/main/java/nc/unc/ama/discovery_evr/DiscoveryEvrApplication.java
@@ -4,13 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
 
 @SuppressWarnings("PMD")
-@Configuration
 @SpringBootApplication(exclude =  {DataSourceAutoConfiguration.class })
 @EnableEurekaServer
 public class DiscoveryEvrApplication {
@@ -19,11 +15,4 @@ public class DiscoveryEvrApplication {
         SpringApplication.run(DiscoveryEvrApplication.class, args);
     }
 
-    @EnableWebSecurity
-    /* default */static class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-        @Override
-        protected void configure(HttpSecurity http) throws Exception {
-            http.csrf().disable();
-        }
-    }
 }
