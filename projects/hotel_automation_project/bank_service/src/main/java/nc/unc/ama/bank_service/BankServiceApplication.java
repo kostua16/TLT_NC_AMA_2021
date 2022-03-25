343c4035e83c31d3c637044687291ddbd2d6dc70
Fiyri44 <vlad19802211@yandex.ru>
2022-03-25 15:54:52 +0300

    feat: DTO,REST,Feign,Bank #78

--- projects/hotel_automation_project/bank_service/src/main/java/nc/unc/ama/bank_service/BankServiceApplication.java
+++ projects/hotel_automation_project/bank_service/src/main/java/nc/unc/ama/bank_service/BankServiceApplication.java
@@ -2,8 +2,10 @@ package nc.unc.ama.bank_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SuppressWarnings("PMD")
+@EnableEurekaClient
 @SpringBootApplication
 public class BankServiceApplication {
 
