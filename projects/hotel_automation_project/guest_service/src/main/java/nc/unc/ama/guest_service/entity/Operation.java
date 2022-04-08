9664db2bc84810e5a39961a207c49f2409f27e29
qwertyzzzzz <57785323+DcoldD@users.noreply.github.com>
2022-04-08 17:32:18 +0400

    docs(GuestService): Документация для модуля + dependencyManagement(javadoc-plugin) #103

--- projects/hotel_automation_project/guest_service/src/main/java/nc/unc/ama/guest_service/entity/Operation.java
+++ projects/hotel_automation_project/guest_service/src/main/java/nc/unc/ama/guest_service/entity/Operation.java
@@ -4,6 +4,11 @@ import lombok.*;
 
 import javax.persistence.*;
 
+/**
+ * Класс услуг со свойствами <b>idOperation</b>, <b>operationName</b>, <b>description</b> и <b>price</b> .
+ * @autor Курочкин Владислав
+ */
+
 @Getter
 @Setter
 @NoArgsConstructor
@@ -12,20 +17,32 @@ import javax.persistence.*;
 
 public class Operation {
 
+    /** Поле id услуги */
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(name = "id")
     private int idOperation;
 
+    /** Поле название услуги */
     @Column(name = "operationname")
     private String operationName;
 
+    /** Поле описание услуги */
     @Column(name = "description")
     private String description;
 
+    /** Поле цена */
     @Column(name = "price")
     private int price;
 
+
+    /**
+     * Конструктор - создание нового объекта с определенными значениями
+     * @param idOperation - id услуги
+     * @param operationName - название услуги
+     * @param description - описание услуги
+     * @param price - цена
+     */
     @Builder
     public Operation(int idOperation, String operationName, String description, int price) {
         this.idOperation = idOperation;
