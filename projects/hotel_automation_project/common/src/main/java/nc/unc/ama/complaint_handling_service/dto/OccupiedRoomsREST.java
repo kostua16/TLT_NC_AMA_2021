5783cc87959e0cfc5de7f2f90dc67f2abe064438
Konstantin Leontiev <konstantin.leontiev@netcracker.com>
2022-04-12 19:47:05 +0400

    fix: prepare routes for simple api to show on the next lection
    Took 17 minutes

--- projects/hotel_automation_project/common/src/main/java/nc/unc/ama/complaint_handling_service/dto/OccupiedRoomsREST.java
+++ projects/hotel_automation_project/common/src/main/java/nc/unc/ama/complaint_handling_service/dto/OccupiedRoomsREST.java
@@ -3,7 +3,7 @@ package nc.unc.ama.complaint_handling_service.dto;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@FeignClient(name = "booking", path = "booking_service")
+@FeignClient(name = "booking", path = "/rooms")
 public interface OccupiedRoomsREST {
 
     @GetMapping("/getOccupiedRoom")
--- projects/hotel_automation_project/common/src/main/java/nc/unc/ama/complaint_handling_service/dto/OperationREST.java
+++ projects/hotel_automation_project/common/src/main/java/nc/unc/ama/complaint_handling_service/dto/OperationREST.java
@@ -1,11 +1,10 @@
 package nc.unc.ama.complaint_handling_service.dto;
 
+import java.util.List;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-
-import java.util.List;
 
 @FeignClient(name = "guest_service", path = "/api/operations")
 public interface OperationREST {
@@ -21,4 +20,5 @@ public interface OperationREST {
 
     @DeleteMapping("/api/operations/{idOperation}")
     void deleteOperation(int idOperation);
+
 }
