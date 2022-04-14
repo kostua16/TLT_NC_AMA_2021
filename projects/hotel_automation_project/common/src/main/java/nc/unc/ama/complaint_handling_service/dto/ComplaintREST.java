d300cc6a117f62a6358d067641d34bdc1d9a980f
Konstantin Leontiev <konstantin.leontiev@netcracker.com>
2022-04-14 13:25:46 +0400

    feat: make feign clients to be optional beans
    Took 1 hour 27 minutes

--- projects/hotel_automation_project/common/src/main/java/nc/unc/ama/complaint_handling_service/dto/ComplaintREST.java
+++ projects/hotel_automation_project/common/src/main/java/nc/unc/ama/complaint_handling_service/dto/ComplaintREST.java
@@ -1,13 +1,15 @@
 package nc.unc.ama.complaint_handling_service.dto;
 
 import java.util.List;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-@FeignClient(name = "complain_handling", path = "/complaint")
+@FeignClient(name = "complaints", path = "/complaint")
+@ConditionalOnProperty(prefix = "app.clients", name = "complaintsApi")
 public interface ComplaintREST {
 
     @PostMapping("/createComplaint")
--- projects/hotel_automation_project/common/src/main/java/nc/unc/ama/complaint_handling_service/dto/OperationREST.java
+++ projects/hotel_automation_project/common/src/main/java/nc/unc/ama/complaint_handling_service/dto/OperationREST.java
@@ -1,12 +1,14 @@
 package nc.unc.ama.complaint_handling_service.dto;
 
 import java.util.List;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @FeignClient(name = "guest_service", path = "/api/operations")
+@ConditionalOnProperty(prefix = "app.clients", name = "operationsApi")
 public interface OperationREST {
 
     @GetMapping("/api/operations/")
