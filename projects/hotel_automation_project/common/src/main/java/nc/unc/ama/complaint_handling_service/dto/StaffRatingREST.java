39fc5994cdb5740fc9960e6b2f7fe9577d35b156
DcoldD <Krutobok1214>
2022-05-02 23:46:39 +0400

    fix(StaffService): Staff исправление замечаний #125

--- projects/hotel_automation_project/common/src/main/java/nc/unc/ama/complaint_handling_service/dto/StaffRatingREST.java
+++ projects/hotel_automation_project/common/src/main/java/nc/unc/ama/complaint_handling_service/dto/StaffRatingREST.java
@@ -2,7 +2,10 @@ package nc.unc.ama.complaint_handling_service.dto;
 
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @FeignClient(name = "staff_service", path = "/staff-rating")
 @ConditionalOnProperty(prefix = "app.clients", name = "staffRatingApi")
