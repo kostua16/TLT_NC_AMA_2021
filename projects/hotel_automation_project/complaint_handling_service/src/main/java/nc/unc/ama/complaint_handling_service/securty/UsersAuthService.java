0823f8fc47d925b69d8457aae560ad5ff51f4b30
Konstantin Leontiev <konstantin.leontiev@netcracker.com>
2022-05-20 04:09:09 +0400

    fix: поправили UsersAuthService - теперь bean
    Took 4 minutes

--- projects/hotel_automation_project/complaint_handling_service/src/main/java/nc/unc/ama/complaint_handling_service/securty/UsersAuthService.java
+++ projects/hotel_automation_project/complaint_handling_service/src/main/java/nc/unc/ama/complaint_handling_service/securty/UsersAuthService.java
@@ -15,8 +15,10 @@ import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
 @SuppressWarnings("PMD.AvoidUncheckedExceptionsInSignatures")
+@Service
 public class UsersAuthService implements UserDetailsService {
 
     private final UsersREST usersREST;
